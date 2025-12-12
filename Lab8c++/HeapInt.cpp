561cf80d0e6861749ade49b0c0ccc9951705f165
sevcenconik07-afk <sevcenconik07@gmail.com>
2025-12-12 14:53:20 +0200

    Update HeapInt.cpp

--- Lab8c++/HeapInt.cpp
+++ Lab8c++/HeapInt.cpp
@@ -1,35 +1,49 @@
+
 #include <iostream>
 #include "HeapInt.h"
 
 HeapInt::HeapInt(const HeapInt& hello) 
 {
-	this->heapValue = new int{ *hello.heapValue };
-	delete hello.heapValue;
+	
+	
+        *this->heapValue = new int{ *hello.heapValue} ;
+          
 }
  void HeapInt::operator=(const HeapInt& hello) 
  {
-	this->heapValue = new int{ *hello.heapValue };
-	 delete hello.heapValue;
-	
+     if(this->heapValue != nullptr )
+     {
+         
+     	*this->heapValue = *hello.heapValue ;
+     }
+     else
+     {
+        *this->heapValue = new int{ *hello.heapValue} ;
+     }
  }
 HeapInt::HeapInt(HeapInt&& hello) 
 {
-	this->heapValue = std::move(hello.heapValue);
-	delete hello.heapValue;
+	    
+        this->heapValue = hello.heapValue;
+        hello.heapValue = nullptr;
+     
 }
 void HeapInt::operator =(HeapInt&& hello) 
 {
-	this->heapValue = std::move(hello.heapValue);
-	delete hello.heapValue;
+     delete  this->heapValue ;
+	 this->heapValue = hello.heapValue;
+	 hello.heapValue = nullptr;
 	
 }
 void swap(HeapInt& a, HeapInt& b)
 {
 	HeapInt temp{ std::move(a) };
-
+    
 	a = std::move(b);
+    
 	b = std::move(temp);
-	delete temp.asRef();
+    
+	 
 			
 }
 
@@ -37,7 +51,7 @@ void swap(HeapInt& a, HeapInt& b)
 
 int main() 
 {
-	
-	
+
+
 }
 
